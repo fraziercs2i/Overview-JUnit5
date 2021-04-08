ecaa9b449adfce6b786d3cf905c87b1fb951b134
fraziercs2i <jgwenet@hotmail.com>
2021-04-08 03:21:29 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/GreatingTest.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreatingTest {
+
+    @org.junit.jupiter.api.Test
+    void helloWorld() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHelloWorld() {
+    }
+}
